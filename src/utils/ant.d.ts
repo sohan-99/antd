e97480c71f7c5b47e25e1dc27f7498afe3c92650
sohan-99 <sohan75632@gmail.com>
2025-08-21 12:51:45 +0600

    feat: implement SlideBer component and update Ant Design theme configuration

---
+++ src/utils/ant.d.ts
@@ -0,0 +1,28 @@
+// src/antd.d.ts
+import "antd/es/theme/interface";
+
+declare module "antd/es/theme/interface" {
+  export interface AliasToken {
+    primary50?: string; // custom name gulo ? diye optional kora hoise jeno type error na dhore
+    primary100?: string;
+    primary200?: string;
+    primary300?: string;
+    primary400?: string;
+    primary500?: string;
+    primary600?: string;
+    primary700?: string;
+    primary800?: string;
+    primary900?: string;
+
+    secondary50?: string;
+    secondary100?: string;
+    secondary200?: string;
+    secondary300?: string;
+    secondary400?: string;
+    secondary500?: string;
+    secondary600?: string;
+    secondary700?: string;
+    secondary800?: string;
+    secondary900?: string;
+  }
+}
--- src/utils/antd.config.ts
+++ src/utils/antd.config.ts
@@ -2,17 +2,48 @@ import { ThemeConfig, theme } from 'antd';
 
 export const antdTheme: ThemeConfig = {
   algorithm: theme.darkAlgorithm,
+
   token: {
-    colorPrimary: '#7C5CFC', 
-    colorBgLayout: '#1D1178', 
-    fontSize: 14,
-    borderRadius: 4,
+    // === overwrite AntD defaults ===
+    colorPrimary: "#7C5CFC", // primary-500
+    colorInfo: "#3b82f6",
+    colorSuccess: "#22c55e",
+    colorWarning: "#f59e0b",
+    colorError: "#ef4444",
+
+    // === primary palette ===
+    primary50: "#eff6ff",
+    primary100: "#E7DEFE",
+    primary200: "#CEBEFE",
+    primary300: "#B49DFE",
+    primary400: "#9F84FD",
+    primary500: "#7C5CFC",
+    primary600: "#5E43D8",
+    primary700: "#432EB5",
+    primary800: "#2D1D92",
+    primary900: "#1D1178",
+
+    // === secondary palette ===
+    secondary50: "#fdf2f8",
+    secondary100: "#F3F5F7",
+    secondary200: "#C3D4E9",
+    secondary300: "#90A3BF",
+    secondary400: "#596780",
+    secondary500: "#1A202C",
+    secondary600: "#131825",
+    secondary700: "#0D121F",
+    secondary800: "#080C19",
+    secondary900: "#040815",
+
+    borderRadius: 8,
+    fontSize: 16,
+
   },
   components: {
     Layout: {
-      headerBg: '#FFFFFF', 
-      footerBg: '#FFFFFF', 
-      bodyBg: '#FFFFFF', 
+      headerBg: '#FFFFFF',
+      footerBg: '#FFFFFF',
+      bodyBg: '#FFFFFF',
 
     },
     Menu: {
